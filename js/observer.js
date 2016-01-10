a39e774795967088d83fc0cd383acceab3580f1f
Suyogya Shrestha <suyogyashrestha@gmail.com>
2016-01-10 16:26:53 -0600

    Observer pattern to use in the timer and a basic HTML layout for the timer

---
+++ js/observer.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let global = (function (){return this;}());
+
+class Publisher{
+	constructor(){
+		this._subscribers = {}
+	}
+
+	subscribe(fn, context=global, type='any'){
+		let subscribers = this._subscribers[type]
+		if(subscribers === undefined){
+			subscribers[type] = [];
+		}
+
+		subscribers[type].push({method:fn, context: context});
+	}
+
+	unsubscribe(fn, context=global, type='any'){
+		let subscribers = this._subscribers[type];
+
+		subscribers.forEach(function(subscriber, index, array){
+			if(subscriber === fn){
+				array.splice(index, 1);
+			}
+		});
+	}
+
+	publish(arg, type='any'){
+		let subscribers = this.subscribers[type];
+
+		for(subscriber of subscribers){
+			let method = subscriber.method;
+			let context = subscriber.context;
+			method.apply(context, arg);
+		}
+	}
+}

a68984ebbc8e6be16fb9dd47386c38e247afde4b
Suyogya Shrestha <suyogyashrestha@gmail.com>
2016-01-17 17:17:13 -0600

    Adding eslint and jscs to gulp task

--- gulpFile.js
+++ gulpFile.js
@@ -1,5 +1,22 @@
 var gulp = require('gulp');
+var jscs = require('gulp-jscs');
+var eslint = require('gulp-eslint');
 
-gulp.task('hello-world', function (){
-	console.log('Our first hello world gulp task!');
-})
+gulp.task('vet', function() {
+	return gulp
+		.src([
+			'./src/**/*.js'
+		])
+		.pipe(jscs({fix:true}))
+		.pipe(jscs.reporter());
+});
+
+gulp.task('lint', function() {
+	return gulp
+		.src([
+			'./src/**/*.js'
+		])
+		.pipe(eslint())
+		.pipe(eslint.format('unix'))
+		.pipe(eslint.failAfterError());
+});

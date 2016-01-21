0c94b71400847aebc646c60a14174e08960abbd1
Suyogya Shrestha <suyogyashrestha@gmail.com>
2016-01-21 16:55:14 -0600

    Added some tests for Timer class

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,7 +1,31 @@
 import Publisher from "Publisher";
 
-class Timer extends Publisher{
-    constructor(){
-        super();
+var Timer = (function () {
+    var updateCurrentTime = Symbol();
+    
+    class Timer extends Publisher {
+        constructor() {
+            super();
+        }
+
+        get currentTime() {
+            return this._currentTime;
+        }
+
+        get totalTime() {
+            return this._totalTime;
+        }
+
+        set totalTime(value) {
+            this._totalTime = value >= 0 ? value : 0;
+            this[updateCurrentTime](this._totalTime);
+        }
+        
+        [updateCurrentTime](value){
+            this._currentTime = value;
+        }
     }
-}
+    return Timer;
+} ());
+
+export default Timer
--- test/timer_test.js
+++ test/timer_test.js
@@ -1,7 +1,39 @@
 import Timer from "js/timer.js";
 
-describe("A test suite", function (){
-    it('should fail', function (){
-        expect(true).to.be.false;
-    })
-})
+describe("Timer class test suite", function (){
+    let timer;
+    
+    before('Testing timer', function (){
+        timer = new Timer();
+    });
+    
+    it('should be a timer object', function (){
+        
+        timer.should.be.a.Timer
+    });
+    
+    it('should have a writable totalTime property', function (){
+        timer.should.have.property('totalTime');
+        
+        timer.totalTime = 50;
+        timer.totalTime.should.be.equal(50);
+    });
+    
+    it('should set totalTime to zero when totalTime is negative', function (){
+        timer.totalTime = -100;
+        timer.totalTime.should.be.equal(0);
+    });
+    
+    it('should have a read only currentTime property', function (){
+        timer.should.have.property('currentTime');
+        (()=>timer.currentTime = 60).should.throw(Error);
+    });
+    
+    it('should set currentTime to totalTime when totalTime is updated', function (){
+        timer.totalTime = 100;
+        timer.currentTime.should.be.equal(100);
+        
+        timer.totalTime = 200;
+        timer.currentTime.should.be.equal(200);
+    });
+});
